720006db8d16a4526cecaea43730c63fb8914282
RomanKitsenko <100saitov@gmail.com>
2021-11-05 23:02:53 +0300

    fix mistake)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let password = "ghg_11217";
+let pass = "-cxcc";
 
-if ((password.length > 4 && password.includes("-")) || password.includes("_")) {
+if (pass.length > 4 && (pass.includes("-") || pass.includes("_"))) {
   console.log("Пароль достаточно надежный");
 } else {
   console.log("Пароль недостаточно надежный");
